2c61d8ea954874e6555a071d5159bdfd6e5451d6
Hamesz <j.hanratty98@gmail.com>
2023-02-19 21:36:05 +0100

    implement generic code generation methods

--- src/generator.compiler.ts
+++ src/generator.compiler.ts
@@ -1,62 +1,141 @@
-import { ClassDeclaration, Project, Scope, SourceFile } from 'ts-morph';
+import {
+  ClassDeclaration,
+  MethodDeclaration,
+  ModuleDeclaration,
+  ModuleDeclarationKind,
+  Project,
+  Scope,
+  SourceFile,
+} from 'ts-morph';
+
 export function run() {
   const project = new Project();
   const sourceFile = project.createSourceFile(`/Users/james/IdeaProjects/routes-schema/src/file.ts`);
 
-  const classDeclaration = sourceFile.addClass({
-    name: 'SomeClass',
-  });
+  const classDefinition2: ICustomClass = {
+    name: 'CustomClass2',
+    customMethods: [],
+    classConstructor: undefined,
+  };
+  const customNamespaceForClass2: ICustomNamespace = {
+    name: 'CustomNamespace2',
+    customSegmentStructures: [],
+  };
+  const segmentStructure2: ICustomSegmentStructure = {
+    class: classDefinition2,
+    namespace: customNamespaceForClass2,
+  };
 
-  const constr = classDeclaration.addConstructor({});
+  const classDefinition1: ICustomClass = {
+    name: 'CustomClass1',
+    customMethods: [],
+    classConstructor: undefined,
+  };
+  const customNamespaceForClass1: ICustomNamespace = {
+    name: 'CustomNamespace1',
+    customSegmentStructures: [segmentStructure2],
+  };
+  const segmentStructure1: ICustomSegmentStructure = {
+    class: classDefinition1,
+    namespace: customNamespaceForClass1,
+  };
 
-  constr.setBodyText('this.myProp = myProp');
-
-  classDeclaration.addProperty({
-    name: 'myProp',
-    type: 'string',
-    initializer: "'hello world!'",
-    scope: Scope.Public,
-  });
-  sourceFile.formatText();
+  const file = createRoutes(sourceFile, segmentStructure1);
+  file.formatText();
   // tslint:disable-next-line:no-console
-  console.log(sourceFile.getText());
+  console.log(file.getText());
+  file.save();
 }
 
-interface RouteSegmentClass {
-  name: string;
-  path: string;
-}
-function createClass(sourceFile: SourceFile, routeSegmentClass: RouteSegmentClass): void {
-  const classDeclaration = sourceFile.addClass({
-    name: routeSegmentClass.name,
+export function createMethod(classDeclaration: ClassDeclaration, methodDefinition: ICustomMethod): MethodDeclaration {
+  const methodDeclaration = classDeclaration.addMethod({
+    name: methodDefinition.name,
   });
+  methodDeclaration.addParameters(
+    methodDefinition.args.map((arg) => {
+      return {
+        name: arg,
+        type: 'any',
+      };
+    }),
+  );
+
+  if (!!methodDefinition.returnType) {
+    methodDeclaration.setReturnType(methodDefinition.returnType);
+  }
+
+  const fromArguments = ['this', ...methodDefinition.args].join(',');
+  methodDeclaration.setBodyText((writer) =>
+    writer.writeLine(`return ${methodDefinition.returnType}.from(${fromArguments});`),
+  );
+
+  return methodDeclaration;
 }
 
-interface IFunction {
-  name: string;
-  path: string;
-}
-function createMethod(classDeclaration: ClassDeclaration, functionDefinition: IFunction): void {
-  classDeclaration.addMethod({
-    name: functionDefinition.name,
+export function createClass(moduleDeclaration: ModuleDeclaration | SourceFile, classDefinition: ICustomClass) {
+  const classDeclaration = moduleDeclaration.addClass({
+    name: classDefinition.name,
   });
+  if (classDefinition.parentClass?.name) {
+    classDeclaration.setExtends(classDefinition.parentClass?.name);
+  }
+  if (classDefinition.classConstructor) {
+    const con = classDeclaration.addConstructor();
+    con.addParameters(
+      classDefinition.classConstructor.args.map((arg) => {
+        return {
+          name: arg,
+          type: 'any',
+        };
+      }),
+    );
+    con.setBodyText('super();');
+  }
+  return classDeclaration;
 }
 
-interface ICustomMethod {
+export function createRoutes(sourceFile: SourceFile, rootSegmentStructure: ICustomSegmentStructure) {
+  createSegmentStructure(sourceFile, rootSegmentStructure);
+  return sourceFile;
+}
+
+export function createNamespace(
+  moduleDeclaration: ModuleDeclaration | SourceFile,
+  namespaceDefinition: ICustomNamespace,
+) {
+  const namespaceDeclaration = moduleDeclaration.addModule({
+    name: namespaceDefinition.name,
+  });
+  namespaceDeclaration.setDeclarationKind(ModuleDeclarationKind.Namespace);
+
+  namespaceDefinition.customSegmentStructures.forEach((seg) => createSegmentStructure(namespaceDeclaration, seg));
+  return namespaceDeclaration;
+}
+
+export function createSegmentStructure(
+  moduleDeclaration: ModuleDeclaration | SourceFile,
+  segmentStructure: ICustomSegmentStructure,
+) {
+  const classDeclaration = createClass(moduleDeclaration, segmentStructure.class);
+  const namespaceDeclaration = createNamespace(moduleDeclaration, segmentStructure.namespace);
+}
+
+export interface ICustomMethod {
   name: string;
-  returnType: string;
+  returnType?: string; // constructors do not have return types
   args: string[];
 }
-interface ICustomClass {
+export interface ICustomClass {
   name: string;
-  constructor: ICustomMethod;
-  customMethods: ICustomMethod;
+  customMethods: ICustomMethod[];
+  classConstructor?: ICustomMethod;
+  parentClass?: ICustomClass;
 }
-interface ICustomNamespace {
+export interface ICustomNamespace {
   name: string;
   customSegmentStructures: ICustomSegmentStructure[];
 }
-interface ICustomSegmentStructure {
+export interface ICustomSegmentStructure {
   class: ICustomClass;
   namespace: ICustomNamespace;
 }
