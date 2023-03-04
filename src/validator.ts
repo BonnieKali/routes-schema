6fc969ff6acedf0069a127241e931d47bae7af6e
Vangelis <axokid318@gmail.com>
2023-03-04 02:06:06 +0100

    fix small mistakes

--- src/validator.ts
+++ src/validator.ts
@@ -1,12 +1,12 @@
 import { ISegmentNode } from './parser';
 import { RESERVED_ROUTES_VALIDATORS } from './exception/reserved-route-segment.exception';
 import { INVALID_URL_VALIDATORS } from './exception/invalid-url-segment.exception';
-import { Optional, RouteSchemaException, RouteSchemaValidator } from './exception/route-schema.exception';
+import { RouteSchemaException, RouteSchemaValidator } from './exception/route-schema.exception';
 import { PATH_VARIABLE_COLLISION_VALIDATORS } from './exception/path-variable-collision.exception';
 import { INVALID_METHOD_NAME_GENERATION_VALIDATORS } from './exception/invalid-method-name.exception';
 
 export function validate(root: ISegmentNode): void {
-  let exceptions: Optional<RouteSchemaException>[] = [];
+  let exceptions: RouteSchemaException[] = [];
   const validators: RouteSchemaValidator[] = [
     ...RESERVED_ROUTES_VALIDATORS,
     ...INVALID_URL_VALIDATORS,
@@ -14,16 +14,16 @@ export function validate(root: ISegmentNode): void {
     ...INVALID_METHOD_NAME_GENERATION_VALIDATORS,
   ];
   function validateNode(node: ISegmentNode) {
-    exceptions = exceptions.concat(validators.map((validator) => validator(node, '')));
+    const newExceptions = validators
+      .map((validator) => validator(node, '')) // TODO: pass in path
+      .filter((e) => !!e) as RouteSchemaException[];
+    exceptions.push(...newExceptions);
     node.children.forEach((child) => validateNode(child));
   }
 
   validateNode(root);
 
-  exceptions
-    .filter((e) => !e)
-    // @ts-ignore
-    .forEach((e: RouteSchemaException) => {
-      console.error(e.toString);
-    });
+  exceptions.forEach((e: RouteSchemaException) => {
+    console.error(e.toString());
+  });
 }
